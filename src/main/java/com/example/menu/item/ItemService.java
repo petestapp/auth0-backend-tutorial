5aae3d5fdca7ca656ec4701408ff9433ddecd3ab
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-21 15:59:17 -0500

    added defaultItems()

--- src/main/java/com/example/menu/item/ItemService.java
+++ src/main/java/com/example/menu/item/ItemService.java
@@ -4,6 +4,8 @@ import org.springframework.data.map.repository.config.EnableMapRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @EnableMapRepositories
 public class ItemService {
@@ -11,5 +13,14 @@ public class ItemService {
 
     public ItemService(CrudRepository<Item, Long> repository) {
         this.repository = repository;
+        this.repository.saveAll(defaultItems());
+    }
+
+    private static List<Item> defaultItems() {
+        return List.of(
+                new Item(1L, "Burger", 599L, "Tasty", "https://cdn.auth0.com/blog/whatabyte/burger-sm.png"),
+                new Item(2L, "Pizza", 299L, "Cheesy", "https://cdn.auth0.com/blog/whatabyte/pizza-sm.png"),
+                new Item(3L, "Tea", 199L, "Informative", "https://cdn.auth0.com/blog/whatabyte/tea-sm.png")
+        );
     }
 }
