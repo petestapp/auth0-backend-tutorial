b032aab5051b64a62e01c1c2152c9e68681638f0
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-21 16:37:14 -0500

    created get requests

--- src/main/java/com/example/menu/item/ItemController.java
+++ src/main/java/com/example/menu/item/ItemController.java
@@ -1,7 +1,13 @@
 package com.example.menu.item;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/menu/items")
@@ -10,5 +16,17 @@ public class ItemController {
 
     public ItemController(ItemService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> findAll() {
+        List<Item> items = service.findAll();
+        return ResponseEntity.ok().body(items);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> find(@PathVariable("id") Long id) {
+        Optional<Item> item = service.find(id);
+        return ResponseEntity.of(item);
     }
 }
