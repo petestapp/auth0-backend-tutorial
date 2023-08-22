3b22c2a165555de13c501ab7148deff2c57d921e
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-22 11:46:11 -0500

    added cross origin decorator

--- src/main/java/com/example/menu/item/ItemController.java
+++ src/main/java/com/example/menu/item/ItemController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "https://dashboard.whatabyte.app")
 @RestController
 @RequestMapping("api/menu/items")
 public class ItemController {
